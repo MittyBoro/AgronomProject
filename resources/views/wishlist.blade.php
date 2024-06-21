2dbb0b093e39431db8320bda2c309c56bb4300c3
Dima Boro <iboro770@gmail.com>
2024-06-21 19:56:49 +0300

    feat: wishlist page html

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -1,6 +1,25 @@
 <x-layouts.main body_name="wishlist">
-  {{--  --}}
-  <section class="__section">
-    <div class="__container container"></div>
+  <x-breadcrumbs :list="[['Каталог', '/catalog'], ['Избранное', null]]" />
+
+  {{-- wishlist --}}
+  <section class="wishlist wishlist__section">
+    <div class="wishlist__container container">
+      <div class="wishlist__title title">
+        <h1>Избранное (8)</h1>
+        <a href="#" class="button button-alt wishlist__button">
+          Добавить все в корзину
+        </a>
+      </div>
+      <div class="wishlist__products products__list">
+        <!-- Promotion Items -->
+        @foreach (range(1, 8) as $product)
+          <x-products.card />
+        @endforeach
+
+        <div class="catalog__pagination">
+          <div class="button">Показать больше</div>
+        </div>
+      </div>
+    </div>
   </section>
 </x-layouts.main>
