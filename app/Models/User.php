d11a233117684670473c92fa172e3352c1fc3cc2
Dima Boro <iboro770@gmail.com>
2024-07-03 11:55:14 +0300

    feat: user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,26 +11,25 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    const GENDER_MALE = 'male';
+    const GENDER_FEMALE = 'female';
+
+    const ROLE_USER = 'user';
+    const ROLE_ADMIN = 'admin';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
