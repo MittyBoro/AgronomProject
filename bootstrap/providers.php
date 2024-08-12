8b1ef4ef1e7b991d021f9296bd69620779d8d4c9
Dima Boro <iboro770@gmail.com>
2024-08-12 11:08:42 +0300

    feat: telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\TheAdminPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class,
 ];
