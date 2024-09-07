d4d8bf47cc86f63648a7e95bc7831495adc37d8d
Dima Boro <iboro770@gmail.com>
2024-09-07 23:57:38 +0300

    ci: queue command

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,37 +6,31 @@ echo  "Deployment started ..."
 # Войти в режим обслуживания или вернуть true
 # если уже находится в режиме обслуживания
 (php artisan down) || true
-echo "Application is not running"
 
 # Извлечь последнюю версию приложения
 git checkout main
 git pull origin main
-echo "Last version deployed"
 
 # Установить зависимости composer
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-echo "Composer dependencies installed"
 
 # Установить зависимости npm
 npm ci
-echo "Node dependencies installed"
 
 # Скомпилировать ресурсы npm
 npm run build
-echo "Node resources builded"
 
 # Очистить старый кэш
 php artisan clear-compiled
 
 # Заново создать кэш
 php artisan optimize
-echo "Cache cleaned"
 
 # Запустить миграцию базы данных
 php artisan migrate --force
-echo "Database migrated"
 
 # Выйти из режима обслуживания
 php artisan up
+php artisan queue:restart
 
 echo  "Deployment done!"
